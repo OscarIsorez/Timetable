075881d9ba2f82f374a926a3fd15235ad21060c9
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-09-11 14:50:36 +0200

    update

--- Emploi_du_temps.py
+++ Emploi_du_temps.py
@@ -1,14 +1,13 @@
 from icalendar import Calendar
-from datetime import  date, timedelta, datetime
+from datetime import date, timedelta, datetime
 import pyperclip
 from random import randint
 import requests
 import os
-#bibliothèque pour récupérer la date d'aujourd'hui et le jour de la semaine
+# bibliothèque pour récupérer la date d'aujourd'hui et le jour de la semaine
 import subprocess
 import pytz
 
-print(type(timedelta(hours=2.5)))
 
 def convertir_heure_gmt_vers_locale(heure_gmt, pays):
     try:
@@ -29,8 +28,9 @@ def convertir_heure_gmt_vers_locale(heure_gmt, pays):
     except Exception as e:
         return f"Erreur : {str(e)}"
 
+
 # L'URL du fichier que vous souhaitez télécharger
-url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9n94DDYP.shu'
+url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/M3MXG9WB.shu'
 
 # Envoyer une requête HTTP GET pour télécharger le fichier
 response = requests.get(url)
@@ -41,66 +41,71 @@ acutal_date = date.today()
 if response.status_code == 200:
     # Obtenir le contenu du fichier
     content = response.content
-    
-    #si le fichier Data.ics existe déjà on le supprime
+
+    # si le fichier Data.ics existe déjà on le supprime
     if os.path.exists("Data.ics"):
         os.remove("Data.ics")
-        
+
     # Écrire le contenu dans un fichier .ics nommé "Data.ics"
     with open('Data.ics', 'wb') as file:
         file.write(content)
-        
+
     print("Le fichier a été téléchargé avec succès et enregistré sous 'Data.ics'.")
 else:
     print("La requête a échoué.")
 
 
-
-#une liste de 50 strings de couleurs en hexadécimal dans la même gamme de couleurs
-color_palette = ["#ffd6ff","#f3ceff","#e7c6ff","#d8beff","#c8b6ff","#c0bbff","#b8c0ff","#bac8ff","#baccff","#bbd0ff", "#bbd6ff","#bbdaff", "#ffd6ff","#f3ceff","#e7c6ff","#d8beff","#c8b6ff","#c0bbff","#b8c0ff","#bac8ff","#baccff","#bbd0ff", "#bbd6ff","#bbdaff"]
+# une liste de 50 strings de couleurs en hexadécimal dans la même gamme de couleurs
+color_palette = ["#ffd6ff", "#f3ceff", "#e7c6ff", "#d8beff", "#c8b6ff", "#c0bbff", "#b8c0ff", "#bac8ff", "#baccff", "#bbd0ff", "#bbd6ff",
+                 "#bbdaff", "#ffd6ff", "#f3ceff", "#e7c6ff", "#d8beff", "#c8b6ff", "#c0bbff", "#b8c0ff", "#bac8ff", "#baccff", "#bbd0ff", "#bbd6ff", "#bbdaff"]
 backup_color_palette = color_palette.copy()
 
 
-def count_events (week_data):
+def count_events(week_data):
     all_events_text = []
     for event in week_data:
         all_events_text.append(event[2])
     return all_events_text
 
-#fonction qui prend en paramètre la date d'aujou'dhui et qui renvoie le jour, le mois et l'année du lundi de la semaine dans laquelle se trouve la date d'aujourd'hui
+# fonction qui prend en paramètre la date d'aujou'dhui et qui renvoie le jour, le mois et l'année du lundi de la semaine dans laquelle se trouve la date d'aujourd'hui
+
+
 def get_monday_date(date):
-    #on récupère le jour de la semaine de la date d'aujourd'hui
+    # on récupère le jour de la semaine de la date d'aujourd'hui
     day = date.weekday()
-    #si le jour est lundi, on renvoie la date d'aujourd'hui
+    # si le jour est lundi, on renvoie la date d'aujourd'hui
     if day == 0:
         return date
-    #sinon on renvoie la date d'aujourd'hui moins le nombre de jours qui sépare la date d'aujourd'hui du lundi de la semaine
+    # sinon on renvoie la date d'aujourd'hui moins le nombre de jours qui sépare la date d'aujourd'hui du lundi de la semaine
     else:
         return date - timedelta(days=day)
+
+
 # Chargez le fichier .ics
 with open('Data.ics', 'rb') as f:
     cal = Calendar.from_ical(f.read())
 
 # Créez un dictionnaire pour stocker les données par jour de la semaine
 week_data = {'Monday': [], 'Tuesday': [],
-            'Wednesday': [], 'Thursday': [], 'Friday': []}
-
-
+             'Wednesday': [], 'Thursday': [], 'Friday': []}
 
 
 # Parcourez les événements du calendrier
 for event in cal.walk('vevent'):
-    
-    #on ne va traiter que les éléments qui ont une date comprise entre le lundi de la semaine acutelle et le vendredi de la semaine actuelle
+
+    # on ne va traiter que les éléments qui ont une date comprise entre le lundi de la semaine acutelle et le vendredi de la semaine actuelle
     if event.get('dtstart').dt.date() >= get_monday_date(date.today()) and event.get('dtstart').dt.date() <= get_monday_date(date.today()) + timedelta(days=4):
 
         summary = event.get('summary')
         location = event.get('location')
-        start_time = convertir_heure_gmt_vers_locale(event.get('dtstart').dt, 'Europe/Paris')
-        end_time = convertir_heure_gmt_vers_locale(event.get('dtend').dt, 'Europe/Paris')
+        start_time = convertir_heure_gmt_vers_locale(
+            event.get('dtstart').dt, 'Europe/Paris')
+        end_time = convertir_heure_gmt_vers_locale(
+            event.get('dtend').dt, 'Europe/Paris')
 
-        #tests : affichage des dates et heures des événements et des summary
-        print(f"start_time : {start_time} ; end_time : {end_time} ; summary : {summary}")
+        # tests : affichage des dates et heures des événements et des summary
+        print(
+            f"start_time : {start_time} ; end_time : {end_time} ; summary : {summary}")
         # Obtenez le nom du jour de la semaine (Lundi, Mardi, etc.)
         day_of_week = start_time.strftime('%A')
 
@@ -125,22 +130,21 @@ end_minute = 30
 # Parcourir les heures de 8h à 20h30 avec un intervalle de 15 minutes
 
 
-#on utilisera la bibliothèque datetime pour créer un objet datetime 
+# on utilisera la bibliothèque datetime pour créer un objet datetime
 current_time = datetime(
-    year= acutal_date.year, month= acutal_date.month, day=acutal_date.day , hour=start_hour, minute=start_minute)
+    year=acutal_date.year, month=acutal_date.month, day=acutal_date.day, hour=start_hour, minute=start_minute)
 
-#mettre les heures que une fois sur deux
+# mettre les heures que une fois sur deux
 one_or_two = True
 
 liste_cours = []
+liste_cours_uniques = []
 
-#----------------------------------GESTION DU CODE HTML--------------------------------------------
+# ----------------------------------GESTION DU CODE HTML--------------------------------------------
 
 
 while current_time.hour < end_hour or (current_time.hour == end_hour and current_time.minute <= end_minute):
     html_table += "<tr>"
-    
-    
 
     # Calculer la plage horaire
     time_range_start = current_time.strftime('%H:%M')
@@ -148,50 +152,42 @@ while current_time.hour < end_hour or (current_time.hour == end_hour and current
     time_range = f"{time_range_start}"
 
     # Mettre les heures sur 2 cellules, une fois sur deux
-    if one_or_two : 
-        
-        if color_palette != [] :
+    if one_or_two:
+
+        if color_palette != []:
             color = color_palette[0]
             color_palette.pop(0)
-        else :
+        else:
             color_palette = backup_color_palette.copy()
             color = color_palette[0]
             color_palette.pop(0)
-        
-        html_table += f"<td rowspan='2' style='border:none;background-color: {color}'>{time_range} --------</td>" 
-        one_or_two = False
-    else :
-        one_or_two = True
 
+        html_table += f"<td rowspan='2' style='border:none;background-color: {color}'>{time_range} --------</td>"
+        one_or_two = False
+    else:
+        one_or_two = True
 
     # Parcourir les jours de la semaine
     for day, events in week_data.items():
         event_texts = []
-        for event_start, event_end, event_desc in events:
 
-            if event_start.time() <= current_time.time() < event_end.time():
-                event_texts.append(event_desc)
-
-        
-
-        # Si plusieurs événements, fusionnez-les dans une seule cellule avec rowspan
         if event_texts:
-            if event_texts[0][0:5]  not in liste_cours:
+            #on vérifie si event_texts[0][0:5] est déjà dans liste_cours et également si les dates de départ et de fin sont les mêmes
+            if e[0:5] not in liste_cours  and event_start.date() == event_end.date():
                 liste_cours.append(event_texts[0][0:5])
-                
-            else:
                 rowspan = len(event_texts)
                 event_text = '<br>'.join(event_texts)
-                #on enleve les espces dans le nom de la classe
+                # on enleve les espces dans le nom de la classe
                 n_classe = event_texts[0][0:4].replace(" ", "")
-          
+                nbr_rowspan = (end_time - start_time) // timedelta(minutes=15)
 
-                html_table += f"<td  class='{n_classe}';>{event_text}</td>"
+                html_table += f"<td  rowspan='{nbr_rowspan} 'class='{n_classe}';>{event_text}</td>"
                 # Ignorer les lignes fusionnées suivantes pour ce cours
                 for _ in range(1, rowspan):
                     html_table += "<tr></tr>"
         else:
-            html_table += "<td style='color: RGBa(128,0,128, 0);background-color: #f1f1f1;border: none;border-radius: 10px;padding: 10px;'>----------------------------------------</td>"  # Cellule vide
+            # Cellule vide
+            html_table += "<td style='color: RGBa(128,0,128, 0);background-color: #f1f1f1;border: none;border-radius: 10px;padding: 10px;'>----------------------------------------</td>"
 
     html_table += "</tr>"
 
@@ -202,17 +198,17 @@ html_page += html_table
 html_page += '</head><body></body></html>'
 
 
-#----------------------------GESTION DES FICHIERS--------------------------------------------
+# ----------------------------GESTION DES FICHIERS--------------------------------------------
 
 
-#on ne garde que les 4 premiers caractères de chaque élément de liste_cours
-liste_cours =  [x[0:4] for x in liste_cours]
+# on ne garde que les 4 premiers caractères de chaque élément de liste_cours
+liste_cours = [x[0:4] for x in liste_cours]
 
-#on supprime le fichier html s'il existe déjà
+# on supprime le fichier html s'il existe déjà
 if os.path.exists("Timetable/index.html"):
     os.remove("Timetable/index.html")
 
-#on crée un fichier html avec le contenu de html_page
+# on crée un fichier html avec le contenu de html_page
 fichier = open("Timetable/index.html", "w")
 fichier.write(html_page)
 fichier.close()
@@ -222,25 +218,25 @@ if os.path.exists("Timetable/style.css"):
     os.remove("Timetable/style.css")
 
 
-#on crée un fichier css avec le contenu de style.css
+# on crée un fichier css avec le contenu de style.css
 fichier = open("Timetable/style.css", "w")
 # pour chaque élément de liste_cours, on crée une classe css avec une couleur dans la liste color_palette
 for i in range(len(liste_cours)):
     if liste_cours[i].count(' ') > 0:
         liste_cours[i] = liste_cours[i].replace(" ", "")
-    fichier.write(f".{liste_cours[i]} {{background-color: {backup_color_palette[i]}; border: none;border-radius: 10px;padding: 10px;}}\n")
+    fichier.write(
+        f".{liste_cours[i]} {{background-color: {backup_color_palette[i]}; border: none;border-radius: 10px;padding: 10px;}}\n")
 fichier.close()
 
 
+# -------------------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------------------
 
-
-#dans le terminal de commande, on lance les commandes suivantes pour mettre à jour le site web
+# dans le terminal de commande, on lance les commandes suivantes pour mettre à jour le site web
 # cd Timetable/
 # git add .
 # git commit -m "update"
-# git push 
+# git push
 
 
 # Spécifiez le répertoire dans lequel vous souhaitez exécuter les commandes Git
@@ -263,6 +259,3 @@ try:
     print("Les commandes Git ont été exécutées avec succès.")
 except subprocess.CalledProcessError as e:
     print("Une erreur s'est produite lors de l'exécution des commandes Git :", e)
-
-
-
