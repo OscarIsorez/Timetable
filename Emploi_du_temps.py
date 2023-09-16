c56dda87f3d398842d6bd118ea73fe384df2ffd0
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-09-16 12:25:22 +0200

    v6

--- Emploi_du_temps.py
+++ Emploi_du_temps.py
@@ -28,12 +28,12 @@ def convertir_heure_gmt_vers_locale(heure_gmt, pays):
         return f"Erreur : {str(e)}"
 
 
-
-
 """ 
     fonction qui prend en paramètre la liste des événements de la semaine et
     qui renvoie la liste des textes des événements de la semaine
 """
+
+
 def count_events(week_data):
     all_events_text = []
     for event in week_data:
@@ -45,6 +45,8 @@ def count_events(week_data):
     fonction qui prend en paramètre la date d'aujourd'hui et
     qui renvoie la date du lundi de la semaine dans laquelle se trouve la date d'aujourd'hui
 """
+
+
 def get_monday_date(date):
     # on récupère le jour de la semaine de la date d'aujourd'hui
     day = date.weekday()
@@ -56,26 +58,25 @@ def get_monday_date(date):
         return date - timedelta(days=day)
 
 
-
 """
     fonction qui initialise le dictionnaire week_data avec les événements de la semaine
     Exemple : week_data = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
 """
+
+
 def treat_data(date_to_treat):
 
-    
     # Créez un dictionnaire pour stocker les données par jour de la semaine
     week_data = {'Monday': [], 'Tuesday': [],
-                'Wednesday': [], 'Thursday': [], 'Friday': []}
+                 'Wednesday': [], 'Thursday': [], 'Friday': []}
 
     # Parcourez les événements du calendrier
     for event in cal.walk('vevent'):
         # si la date dstat est comprise entre date_to_treat et date_to_treat + 4 jours
-        if  event.get('dtstart').dt.date() >= date_to_treat and event.get('dtstart').dt.date() <=date_to_treat + timedelta(days=4):
-        # if event.get('dtstart').dt.date() >= date_to_treat and event.get('dtstart').dt.date() <=date_to_treat + timedelta(days=4):
+        if event.get('dtstart').dt.date() >= date_to_treat and event.get('dtstart').dt.date() <= date_to_treat + timedelta(days=4):
+            # if event.get('dtstart').dt.date() >= date_to_treat and event.get('dtstart').dt.date() <=date_to_treat + timedelta(days=4):
             # print("date_to_treat", date_to_treat)
             # print("date_to_treat+4", date_to_treat + timedelta(days=4))
-
 
             # Obtenez les informations de l'événement
             summary = event.get('summary')
@@ -97,8 +98,9 @@ def treat_data(date_to_treat):
             # Ajoutez l'événement au dictionnaire de données correspondant au jour de la semaine
             week_data[day_of_week].append((start_time, end_time, event_texts))
 
-            #on affiche tous les event_texts dans la console
+            # on affiche tous les event_texts dans la console
     return week_data
+
 
 def generate_html_page(date_to_treat, color_palette, file_name, to_page):
 
@@ -106,12 +108,12 @@ def generate_html_page(date_to_treat, color_palette, file_name, to_page):
 
     # Créez un tableau HTML
     html_table = ""
-
-
-
-
     html_table += "<table border='1'>"
-    html_table += f"<tr><th class='first_column'>Plage Horaire</th><th style='border: none; background-color:{color_palette[0]};'>Lundi</th><th style='border: none;background-color: {color_palette[1]};'>Mardi</th><th style='border: none;background-color: {color_palette[2]};'>Mercredi</th><th style='border: none;background-color: {color_palette[3]};'>Jeudi</th><th  style='border: none;background-color: {color_palette[4]};'>Vendredi</th></tr>"
+
+    top_left = str(date_to_treat.day) + " \nau " + str((date_to_treat + timedelta(days=4)).day) + " 0" + \
+        str((date_to_treat + timedelta(days=4)).month) + \
+        "-" + str((date_to_treat + timedelta(days=4)).year)
+    html_table += f"<tr><th class='first_column'>Semaine du {top_left}</th><th style='border: none; background-color:{color_palette[0]};'>Lundi</th><th style='border: none;background-color: {color_palette[1]};'>Mardi</th><th style='border: none;background-color: {color_palette[2]};'>Mercredi</th><th style='border: none;background-color: {color_palette[3]};'>Jeudi</th><th  style='border: none;background-color: {color_palette[4]};'>Vendredi</th></tr>"
 
     # Définissez l'heure de début (8h du matin, après avoir ajouté 2 heures)
     start_hour = 8
@@ -123,8 +125,7 @@ def generate_html_page(date_to_treat, color_palette, file_name, to_page):
 
     # Parcourir les heures de 8h à 20h30 avec un intervalle de 15 minutes
 
-
-        # on utilisera la bibliothèque datetime pour créer un objet datetime
+    # on utilisera la bibliothèque datetime pour créer un objet datetime
     current_time = datetime(
         year=acutal_date.year, month=acutal_date.month, day=acutal_date.day, hour=start_hour, minute=start_minute)
 
@@ -134,9 +135,7 @@ def generate_html_page(date_to_treat, color_palette, file_name, to_page):
     liste_cours = []
     liste_cours_uniques = []
 
-
     # ----------------------------------GESTION DU CODE HTML--------------------------------------------
-
 
     while current_time.hour < end_hour or (current_time.hour == end_hour and current_time.minute <= end_minute):
         html_table += "<tr>"
@@ -170,18 +169,20 @@ def generate_html_page(date_to_treat, color_palette, file_name, to_page):
             for event_start, event_end, event_desc in events:
 
                 if event_start.time() <= current_time.time() < event_end.time():
-                    event_informations.append([event_desc, event_start, event_end])
-            
-            #on vérifie si event_informations[0][0:5] est déjà dans liste_cours et également si les dates de départ et de fin sont les mêmes
+                    event_informations.append(
+                        [event_desc, event_start, event_end])
+
+            # on vérifie si event_informations[0][0:5] est déjà dans liste_cours et également si les dates de départ et de fin sont les mêmes
             if event_informations:
-                if event_informations[0] not in liste_cours: 
+                if event_informations[0] not in liste_cours:
                     liste_cours.append(event_informations[0])
                     event_desc = str(event_informations[0][0])
-                    # on enleve les espces dans le nom de la classe   
+                    # on enleve les espces dans le nom de la classe
                     n_classe = event_informations[0][0][0:4].replace(" ", "")
-                    nbr_rowspan = (event_informations[0][2] - event_informations[0][1]) // timedelta(minutes=15)
+                    nbr_rowspan = (
+                        event_informations[0][2] - event_informations[0][1]) // timedelta(minutes=15)
                     html_table += f"<td  rowspan='{nbr_rowspan} 'class='{n_classe}';>{event_desc}</td>"
-                    
+
             else:
                 # Cellule vide
                 html_table += "<td class='empty'>----------------------------------------</td>"
@@ -206,17 +207,17 @@ def generate_html_file_and_css_file(html_page, liste_cours, liste_cours_uniques,
         liste_cours[i][0] = liste_cours[i][0][0:4]
 
     # on supprime les doublons de liste_cours
-    #la liste contenant elle meme des listes, on ne gardera que le premier élément de chaque élément
+    # la liste contenant elle meme des listes, on ne gardera que le premier élément de chaque élément
     liste_cours_uniques = []
     for i in range(len(liste_cours)):
         liste_cours_uniques.append(liste_cours[i][0])
     liste_cours_uniques = list(set(liste_cours_uniques))
 
 
-#--------------------------------------------HTML-----------------------------------------------
+# --------------------------------------------HTML-----------------------------------------------
 
     # on supprime le fichier html s'il existe déjà
-        # Obtenir le répertoire du script
+    # Obtenir le répertoire du script
     script_directory = os.path.dirname(__file__)
 
     # Chemin complet du fichier "index.html"
@@ -230,35 +231,34 @@ def generate_html_file_and_css_file(html_page, liste_cours, liste_cours_uniques,
     except Exception as e:
         print(f"Une erreur s'est produite : {str(e)}")
 
-#--------------------------------------------CSS-----------------------------------------------
+# --------------------------------------------CSS-----------------------------------------------
 
     # Chemin complet du fichier
     chemin_fichier = os.path.join(script_directory, f"{file_name}.css")
     # Écriture du contenu HTML dans le fichier
     try:
-        with open(chemin_fichier, "w") as fichier:        
+        with open(chemin_fichier, "w") as fichier:
             fichier.write("th, td {width: 17vw;}\n")
             fichier.write("td:hover{border:1 px solid};\n")
             fichier.write(".first_column {width:6vw;}\n")
-            fichier.write("table {font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}\n")
-            fichier.write(".empty {color: RGBa(128,0,128, 0);background-color: #f1f1f1;border: none;border-radius: 10px;padding: 10px;}\n")
+            fichier.write(
+                "table {font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}\n")
+            fichier.write(
+                ".empty {color: RGBa(128,0,128, 0);background-color: #f1f1f1;border: none;border-radius: 10px;padding: 10px;}\n")
             fichier.write("#bouton-suivant {position: fixed;bottom: 20px;right: 20px;background-color: #ffffff;color: #8D889EB9;padding: 10px 20px;border: 2px solid #8D889EB9;border-radius: 5px;cursor: pointer; text-align: center;text-decoration: none;display: inline-block;font-size: 16px;border-radius: 7px;transition: background-color 0.3s, border-color 0.3s, color 0.3s;}#bouton-suivant:hover {background-color: #8D889EB9;border-color: #8D889EB9;    color: #ffffff;}\n")
             # fichier.write(".button {background-color: #ffffff;border: 2px solid #8D889EB9;color: #8D889EB9;padding: 10px 20px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;border-radius: 5px;cursor: pointer;transition: background-color 0.3s, border-color 0.3s, color 0.3s;}\n .button:hover {background-color: #8D889EB9;border-color: #8D889EB9;color: #ffffff; }\n")
             for i in range(len(liste_cours_uniques)):
                 # print(liste_cours_uniques[i])
                 # print(i)
                 if liste_cours_uniques[i].count(' ') > 0:
-                    liste_cours_uniques[i] = liste_cours_uniques[i].replace(" ", "")
+                    liste_cours_uniques[i] = liste_cours_uniques[i].replace(
+                        " ", "")
                 # print(liste_cours_uniques[i])
                 fichier.write(
                     f".{liste_cours_uniques[i]} {{background-color: {backup_color_palette[randint(0,len(backup_color_palette) -1)]}; border: none;border-radius: 10px;padding: 10px;}}\n")
         # print(f"Le fichier '{chemin_fichier}' a été créé ou écrasé avec succès.")
     except Exception as e:
         print(f"Une erreur s'est produite : {str(e)}")
-
-
-
-
 
     # -----------------------------------------GIT--------------------------------------------------
 
@@ -269,7 +269,7 @@ def generate_html_file_and_css_file(html_page, liste_cours, liste_cours_uniques,
     git_add_command = ['git', 'add', '.']
 
     # Commande Git : git commit -m "update"
-    git_commit_command = ['git', 'commit', '-m', 'v5']
+    git_commit_command = ['git', 'commit', '-m', 'v6']
 
     # Commande Git : git push
     git_push_command = ['git', 'push']
@@ -286,8 +286,7 @@ def generate_html_file_and_css_file(html_page, liste_cours, liste_cours_uniques,
     return html_page
 
 
-#--------------------------------------------MAIN-----------------------------------------------
-
+# --------------------------------------------MAIN-----------------------------------------------
 
 
 # L'URL du fichier à télécharger disponible sur ADE
@@ -306,7 +305,7 @@ if response.status_code == 200:
 
     script_directory = os.path.dirname(__file__)
 
-    #si le fichier n'existe pas, on le crée dans le même répertoire que le script
+    # si le fichier n'existe pas, on le crée dans le même répertoire que le script
     if not os.path.exists(os.path.join(script_directory, f"Data.ics")):
         with open(os.path.join(script_directory, f"Data.ics"), "w") as fichier:
             fichier.write("")
@@ -318,7 +317,6 @@ if response.status_code == 200:
     with open(chemin_fichier, "wb") as fichier:
         fichier.write(content)
 
-    
     # print("Le fichier a été téléchargé avec succès et enregistré sous 'Data.ics'.")
 else:
     print("La requête a échoué.")
@@ -338,43 +336,47 @@ else:
     print("Le fichier 'Data.ics' n'existe pas dans le répertoire du script.")
 
 
-# Liste des couleurs pour les cours 
-color_palette =[
-    "#F3DE8A",  
+# Liste des couleurs pour les cours
+color_palette = [
+    "#F3DE8A",
     "#F4DB90",
-    "#F4D796",  
+    "#F4D796",
     "#F4D49C",
-    "#F4D0A2",  
+    "#F4D0A2",
     "#F4CDA8",
-    "#F4C9AE",  
+    "#F4C9AE",
     "#F4C5B4",
-    "#F4C1B9",  
-    "#D7B1B2",  
-    "#B9A0AA",  
+    "#F4C1B9",
+    "#D7B1B2",
+    "#B9A0AA",
     "#AB98A6",
     "#9C90A2",
-    "#958CA0", 
+    "#958CA0",
     "#8D889E",
-    "#86849C", 
-    "#7E7F9A"   
+    "#86849C",
+    "#7E7F9A"
 ]
 
-#copie de la liste de couleurs, utile pour réinitialiser la liste de couleurs
+# copie de la liste de couleurs, utile pour réinitialiser la liste de couleurs
 backup_color_palette = color_palette.copy()
 
 
 # SEMAINE COURANTE
 file_name = "index"
 to_page = "index_s2"
-html_and_css = generate_html_page(get_monday_date(date.today()), color_palette, file_name, to_page)
-generate_html_file_and_css_file(html_and_css[0], html_and_css[1], html_and_css[2], html_and_css[3], html_and_css[4], file_name)  
+html_and_css = generate_html_page(get_monday_date(
+    date.today()), color_palette, file_name, to_page)
+generate_html_file_and_css_file(
+    html_and_css[0], html_and_css[1], html_and_css[2], html_and_css[3], html_and_css[4], file_name)
 
 week_data = {}
 
 # SEMAINE SUIVANTE
 file_name = "index_s2"
 to_page = "index"
-html_and_css_semaine2 = generate_html_page(get_monday_date(date.today()) + timedelta(days=7), color_palette, file_name, to_page)
+html_and_css_semaine2 = generate_html_page(get_monday_date(
+    date.today()) + timedelta(days=7), color_palette, file_name, to_page)
 # print(get_monday_date(date.today()) + timedelta(days=7))
 # print(html_and_css_semaine2[0])
-generate_html_file_and_css_file(html_and_css_semaine2[0], html_and_css_semaine2[1], html_and_css_semaine2[2], html_and_css_semaine2[3], html_and_css_semaine2[4], file_name)
+generate_html_file_and_css_file(html_and_css_semaine2[0], html_and_css_semaine2[1],
+                                html_and_css_semaine2[2], html_and_css_semaine2[3], html_and_css_semaine2[4], file_name)
