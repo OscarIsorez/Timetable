5be989e17be947b43625ecb2ec4bfe7b1a506a3e
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-09-27 21:01:36 +0200

    update

--- ics_to_json.py
+++ ics_to_json.py
@@ -2,9 +2,6 @@ import json
 import urllib.request
 import icalendar
 
-
-
-
 """
 
 fonction qui prend en param?tre un lien qui pointe vers un fichier ics et qui retourne les éléments dans le format json
@@ -13,15 +10,14 @@ fonction qui prend en param?tre un lien qui pointe vers un fichier ics et qui re
 def read_ics_file(url):
     try:
         with urllib.request.urlopen(url) as response:
-            return response.read()
-            print("file read")
+            # Lire le contenu en utilisant l'encodage iso-8859-2
+            ics_content = response.read().decode('iso-8859-2')
+            return ics_content
     except Exception as e:
-        return json.dumps({'ERror': str(e)}, indent=4)
+        return json.dumps({'error': str(e)}, indent=4)
 
 def ics_to_json(ics_content):
-    print("ics to json")
     try:
-
         cal = icalendar.Calendar.from_ical(ics_content)
         events = []
 
@@ -35,25 +31,18 @@ def ics_to_json(ics_content):
             }
             events.append(event_data)
 
-        print("json generated")
-        return json.dumps(events, indent=4)
+        return json.dumps(events, indent=4, ensure_ascii=False).encode('iso-8859-2').decode('iso-8859-2')
 
     except Exception as e:
-        return json.dumps({'ERROR': str(e)}, indent=4)
-
-
+        return json.dumps({'error': str(e)}, indent=4)
 
 def generate_json_file(url, file_name):
     ics_content = read_ics_file(url)
     json_content = ics_to_json(ics_content)
-    with open(file_name, 'w') as f:
+    with open(file_name, 'w', encoding='iso-8859-2') as f:
         f.write(json_content)
-        print("file generated")
-
 
 #------
 
 url = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/o35ex53R.shu"
 
-# generate_json_file(url, "timetable.json")
-print()
