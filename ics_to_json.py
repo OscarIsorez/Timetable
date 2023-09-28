95d3a0d8e270134e248a3e9f365ccd0910cefb12
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-09-28 12:13:02 +0200

    update

--- ics_to_json.py
+++ ics_to_json.py
@@ -1,5 +1,5 @@
 import json
-import urllib.request
+import requests
 import icalendar
 
 """
@@ -7,14 +7,54 @@ import icalendar
 fonction qui prend en param?tre un lien qui pointe vers un fichier ics et qui retourne les éléments dans le format json
 """
 
+
 def read_ics_file(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            # Lire le contenu en utilisant l'encodage iso-8859-2
-            ics_content = response.read().decode('iso-8859-2')
-            return ics_content
-    except Exception as e:
-        return json.dumps({'error': str(e)}, indent=4)
+    response = requests.get(url)
+
+    # Récupérer la date d'aujourd'hui
+    acutal_date = date.today()
+
+    # Vérifier si la requete a réussi
+    if response.status_code == 200:
+        # Obtenir le contenu du fichier
+        content = response.content
+
+        script_directory = os.path.dirname(__file__)
+        # si le fichier n'existe pas, on le crée dans le meme répertoire que le script
+
+        if not os.path.exists(os.path.join(script_directory, f"Data.ics")):
+            with open(os.path.join(script_directory, f"Data.ics"), "w") as fichier:
+                fichier.write("")
+
+        # Chemin complet du fichier
+        chemin_fichier = os.path.join(script_directory, f"Data.ics")
+
+        # écriture du contenu dans le fichier
+        with open(chemin_fichier, "wb") as fichier:
+            fichier.write(content)
+        # print("Le fichier a été téléchargé avec succ?s et enregistré sous 'Data.ics'.")
+
+    else:
+        print("La requete a échoué.")
+
+    # Obtenir le répertoire du script
+
+    script_directory = os.path.dirname(__file__)
+
+    # Construire le chemin relatif vers le fichier "Data.ics"
+
+    fichier_relative_path = os.path.join(script_directory, "Data.ics")
+
+    # Vérifier si le fichier existe
+    if os.path.exists(fichier_relative_path):
+
+        # Ouvrir le fichier en lecture
+        with open(fichier_relative_path, "r", encoding="iso-8859-2") as f:
+            cal = Calendar.from_ical(f.read())
+
+    else:   
+        print("Le fichier 'Data.ics' n'existe pas dans le répertoire du script.")
+
 
 def ics_to_json(ics_content):
     try:
@@ -36,13 +76,14 @@ def ics_to_json(ics_content):
     except Exception as e:
         return json.dumps({'error': str(e)}, indent=4)
 
+
 def generate_json_file(url, file_name):
     ics_content = read_ics_file(url)
     json_content = ics_to_json(ics_content)
     with open(file_name, 'w', encoding='iso-8859-2') as f:
         f.write(json_content)
 
-#------
+# ------
+
 
 url = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/o35ex53R.shu"
-
